bcc2c8ba464289947d1445a120f0649ff77ca402
Nabil <nabilghaddari@alumnat.iesdamiahuguet.net>
2025-02-22 22:19:04 +0100

    canvis5

--- Paraula.js
+++ Paraula.js
@@ -1,4 +1,4 @@
-let intentosRestantes = 5;
+let intentsRestants = 5;
 let paraulaadivinar = 'Rapid';
 paraulaadivinar = paraulaadivinar.toUpperCase();
 //Posa la paraula en majuscules
@@ -6,39 +6,41 @@ paraulaadivinar = paraulaadivinar.toUpperCase();
 function comprovarParaula() {
     let paraulai = document.getElementById("paraulaInput").value;
     paraulai = paraulai.toUpperCase();
-//Posa la paraula introduida en majuscules
-    if (paraulaadivinar == paraulai ){
+    //Posa la paraula introduida en majuscules
+    if (paraulaadivinar == paraulai) {
         document.getElementById('paraula').hidden = false;
         // Si son iguals les paraules has guanyat
-        return; // Aixo fa q no funcioni les altres funcionc si es compleix aquesta
+        return; // Aixo fa q no funcioni les altres funcions si es compleix aquesta
     }
-    if (intentosRestantes > 1) {
-        intentosRestantes--;
-        document.getElementById("intents").innerText = 'Intentos restantes: ' + intentosRestantes;
-    }else {
-        alert(`Se han agotado los intentos. La palabra era: ${paraulaadivinar}`);
-        return;
-    }
-
-    if (paraulaadivinar.length == paraulai.length) {
-        // Comprova si la paraula té la mateixa longitud
-
-        for (let posiciódelaparaula = 0; posiciódelaparaula < paraulai.length; posiciódelaparaula++) {
-            //Aumenta les posicions
-            if (paraulai.charAt(posiciódelaparaula) == paraulaadivinar.charAt(posiciódelaparaula)) {
-                alert('verd')
-                // Es coloreja de color verd
-            }
-            else if (paraulaadivinar.includes(paraulai.charAt(posiciódelaparaula))) {
-                alert('taronja fluix');
-                // Es coloreja de color taronja fluix
-            }
-            else {
-                alert('blanc');
-                // Es coloreja de color blanc
-            }
+        if (intentsRestants > 1) {
+            intentsRestants--;
+        } else {
+            alert(`S'han acabat els intents. La paraula era: ${paraulaadivinar}`);
+            return;
         }
-    } else {
-        alert('Error: La paraula ha de tenir 5 lletres.');
-    }
+
+        if (paraulaadivinar.length == paraulai.length) {
+            // Comprova si la paraula té la mateixa longitud
+            let resultat = '';
+
+            for (let posiciódelaparaula = 0; posiciódelaparaula < paraulai.length; posiciódelaparaula++) {
+                //Aumenta les posicions
+                if (paraulai.charAt(posiciódelaparaula) == paraulaadivinar.charAt(posiciódelaparaula)) {
+                    resultat += `<span class="verd">${paraulai.charAt(posiciódelaparaula)}</span>`;
+                    // Es coloreja de color verd
+                } else if (paraulaadivinar.includes(paraulai.charAt(posiciódelaparaula))) {
+                    resultat += `<span class="taronja">${paraulai.charAt(posiciódelaparaula)}</span>`;
+                    // Cada vegada que hi hagi una lletra de color taronja la pinta y el resultat+= fa que si n'hi ha més d'una lletra taronja la pinti també
+                    //span = només fa allò en concret que li hem posat
+                    // Es coloreja de color taronja
+                } else {
+                    resultat += `<span class="blanc">${paraulai.charAt(posiciódelaparaula)}</span>`;
+                    // Es coloreja de color blanc
+                }
+            }
+            document.getElementById('resultat').innerHTML = resultat; // Mostra el resultat
+
+        } else {
+            alert('Error: La paraula ha de tenir 5 lletres.');
+        }
 }
